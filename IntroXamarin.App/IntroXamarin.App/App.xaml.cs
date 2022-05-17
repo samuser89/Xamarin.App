3a99c74a2f9910e9ca376ae3527864d57f775c32
David Santafe <santafedavid01@gmail.com>
2022-05-17 17:06:22 -0500

    Add project files.

---
+++ IntroXamarin.App/IntroXamarin.App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using IntroXamarin.App.Views.Forms;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntroXamarin.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new RegisterGridPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
